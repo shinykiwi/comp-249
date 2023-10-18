066ce0bbc746b4a451a1ce434228aa4bd4f89120
shinykiwi <kafountain5@gmail.com>
2023-10-18 11:55:57 -0400

    a2 start

---
+++ src/a2/Main.java
@@ -0,0 +1,19 @@
+package a2;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public void do_part1(){
+
+       
+
+
+
+
+    }
+}
